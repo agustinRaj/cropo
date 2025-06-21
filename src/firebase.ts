99e1f72cfafc4894d80e8de13b294b0846339563
agustinRaj <agustinraj20@gmail.com>
2025-06-21 23:49:03 +0400

    fire base added by me

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBF5xq8mW7vTb-2VSoQt6xLzqHqI65Zk9w",
+  authDomain: "cropo-497d7.firebaseapp.com",
+  projectId: "cropo-497d7",
+  storageBucket: "cropo-497d7.firebasestorage.app",
+  messagingSenderId: "24476630214",
+  appId: "1:24476630214:web:9b63b90af85eac584d6bc7",
+  measurementId: "G-MYY5TCJ2BN",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
